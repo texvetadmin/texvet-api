0f57a104738f3ac4e3a10cd7fc328bf4c739bc32
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-03 01:43:12 +0300

    added skiped getTemplate function

--- src/controllers/TemplateController.js
+++ src/controllers/TemplateController.js
@@ -21,6 +21,18 @@ class TemplateController {
       return fail(res, err);
     }
   };
+
+  getTemplate = async (req, res) => {
+    try {
+      const template = await this.templateService.getTemplate(req);
+      const response = makePaginatedResponse(req, template);
+
+      return res.status(200).json(response);
+    } catch (err) {
+      logger.error(`[${this.constructor.name}.getTemplate] Error: ${err}`);
+      return fail(res, err);
+    }
+  };
 }
 
 export default new TemplateController();
