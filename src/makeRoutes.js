2270760d0158db329e286537f0849c4d5388e67f
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-03 04:03:47 +0300

    move route

--- src/makeRoutes.js
+++ src/makeRoutes.js
@@ -1,16 +1,14 @@
 import userRouter from './routes/user';
-import emailRouter from './routes/email';
 import userAuthRouter from './routes/user/auth';
 import EmailController from './controllers/EmailController';
 
 const makeRoutes = app => {
   // public routes
   app.use('/public/v1/users', userRouter);
-  app.use('/public/v1', emailRouter);
 
   // authenticated routes
   app.use('/auth/v1/users', userAuthRouter);
-  app.use('/auth/v1/fulfilments/send-email', EmailController.sendEmail);
+  app.post('/auth/v1/fulfilments/send-email', EmailController.sendEmail);
 };
 
 export default makeRoutes;
