4fac3961306eb4a2ad6c803f503392ef29949118
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-03 17:10:36 +0300

    generate-email function for delivering emails via the chatbot backend system implemented

--- src/lambda.js
+++ src/lambda.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-param-reassign */
-
+import AWS from 'aws-sdk';
 import serverless from 'serverless-http';
 import makeApp from './makeApp';
 import makeRoutes from './makeRoutes';
+import logger from './utils/logger';
 
 // make the server
 const app = makeApp([makeRoutes]);
+
+const sqs = new AWS.SQS({ region: process.env.USERPOOL_REGION });
+const QUEUE_URL = `https://sqs.${process.env.USERPOOL_REGION}.amazonaws.com/${process.env.ACCOUNT_ID}/${process.env.DELIVER_EMAIL_QUEUE_NAME}`;
+
 
 const handler = serverless(app, {
   request: (request, event, context) => {
@@ -18,5 +23,26 @@ const handler = serverless(app, {
   },
 });
 
+const generateEmail = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const params = {
+    MessageBody: {
+      subject: '',
+      message: JSON.parse(event.Records[0].body).text,
+    },
+    QueueUrl: QUEUE_URL,
+  };
+
+  sqs.sendMessage(params, (err, data) => {
+    if (err) {
+      logger.error(`[${this.constructor.name}.generateEmail] Error: ${err}`);
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { handler };
+export { handler, generateEmail };
