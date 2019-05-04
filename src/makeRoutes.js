fec06b420f10eb5161623084f8bdfcbb27c8edc5
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-04 22:44:03 +0300

    Revert "/fulfillment/static-resources endpoint implemented"
    This reverts commit c606f8a9ec992107830579c05a1cd06fba9ce4f5.

--- src/makeRoutes.js
+++ src/makeRoutes.js
@@ -3,7 +3,6 @@ import typeRouter from './routes/notification/type';
 import templateRouter from './routes/notification/template';
 import userAuthRouter from './routes/user/auth';
 import typeAuthRouter from './routes/notification/type/auth';
-import fulfilmentsRouter from './routes/fulfilments';
 import fulfilmentsAuthRouter from './routes/fulfilments/auth';
 
 const makeRoutes = app => {
@@ -11,8 +10,7 @@ const makeRoutes = app => {
   app.use('/public/v1/users', userRouter);
   app.use('/public/v1/notifications/types', typeRouter);
   app.use('/public/v1/notifications/templates', templateRouter);
-  app.use('/public/v1//fulfillment', fulfilmentsRouter);
-
+  
 
   // authenticated routes
   app.use('/auth/v1/users', userAuthRouter);
--- src/routes/fulfilments/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import FulfillmentController from '../../../controllers/FulfillmentController';
-
-const router = express.Router();
-
-router.post('/static-resources/:slug', FulfillmentController.sendEmail);
-
-export default router;
