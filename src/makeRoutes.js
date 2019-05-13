cd1a3688eb40f2ee783a8523847697beb7bc23c2
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-13 12:32:57 +0300

    Remove the unused API template code.

--- src/makeRoutes.js
+++ src/makeRoutes.js
@@ -1,20 +1,16 @@
-import userRouter from './routes/user';
 import typeRouter from './routes/notification/type';
 import templateRouter from './routes/notification/template';
-import userAuthRouter from './routes/user/auth';
 import typeAuthRouter from './routes/notification/type/auth';
 import fulfillmentRouter from './routes/fulfillment';
 import fulfillmentAuthRouter from './routes/fulfillment/auth';
 
 const makeRoutes = app => {
   // public routes
-  app.use('/public/v1/users', userRouter);
   app.use('/public/v1/notifications/types', typeRouter);
   app.use('/public/v1/notifications/templates', templateRouter);
   app.use('/public/v1/fulfillments', fulfillmentRouter);
 
   // authenticated routes
-  app.use('/auth/v1/users', userAuthRouter);
   app.use('/auth/v1/notifications/types', typeAuthRouter);
   app.use('/auth/v1/fulfillments', fulfillmentAuthRouter);
 };
