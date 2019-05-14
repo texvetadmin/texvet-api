979bcda0245138b51c16374125b1d024c50c58da
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-14 16:09:00 +0300

    routes changed

--- src/makeRoutes.js
+++ src/makeRoutes.js
@@ -5,19 +5,21 @@ import userAuthRouter from './routes/user/auth';
 import typeAuthRouter from './routes/notification/type/auth';
 import fulfillmentRouter from './routes/fulfillment';
 import fulfillmentAuthRouter from './routes/fulfillment/auth';
+import followUpRouter from './routes/follow-up';
+import followUpRouterAuthRouter from './routes/follow-up/auth';
 
 const makeRoutes = app => {
   // public routes
   app.use('/public/v1/users', userRouter);
   app.use('/public/v1/notifications/types', typeRouter);
   app.use('/public/v1/notifications/templates', templateRouter);
-  app.use('/public/v1/follow-ups', templateRouter);
+  app.use('/public/v1/follow-ups', followUpRouter);
   app.use('/public/v1/fulfillments', fulfillmentRouter);
 
   // authenticated routes
   app.use('/auth/v1/users', userAuthRouter);
   app.use('/auth/v1/notifications/types', typeAuthRouter);
-  app.use('/auth/v1/notifications/', typeAuthRouter);
+  app.use('/auth/v1/follow-ups', followUpRouterAuthRouter);
   app.use('/auth/v1/fulfillments', fulfillmentAuthRouter);
 };
 
