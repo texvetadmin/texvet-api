0e514a1e0e9b5b2e23f4b60d5f1ea199083238c1
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-16 16:59:47 +0300

    email message audit log added

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk';
 import logger from '../utils/logger';
+import EmailMessageLog from '../models/emailMessageLog';
 
 const sqs = new AWS.SQS({ region: process.env.USERPOOL_REGION });
 
@@ -9,9 +10,21 @@ class EmailService {
   sendEmail = async req => {
     const { message } = req.body;
 
+    const emailLog = new EmailMessageLog({
+      initialRequestType: 'CHATBOT',
+      initialRequestDate: new Date(),
+      initialRequest: req.body,
+      generateEmailMessageDate: null,
+      generateEmailMessage: null,
+      deliverEmailMessageDate: null,
+      deliverEmailMessage: null,
+    });
+    emailLog.save();
+
     const params = {
       MessageBody: message,
       QueueUrl: QUEUE_URL,
+      emailLogId: emailLog._id,
     };
 
     sqs.sendMessage(params, (err, data) => {
--- src/services/SQSService.js
+++ src/services/SQSService.js
@@ -5,21 +5,35 @@ import sgMail from '@sendgrid/mail';
 import logger from '../utils/logger';
 import NotificationTypeModel from '../models/notificationType';
 import NotificationTemplateModel from '../models/notificationTemplate';
+import EmailMessageLog from '../models/emailMessageLog';
 
 const sqs = new AWS.SQS({ region: process.env.USERPOOL_REGION });
 const QUEUE_URL = `https://sqs.${process.env.USERPOOL_REGION}.amazonaws.com/${process.env.ACCOUNT_ID}/${process.env.DELIVER_EMAIL_QUEUE_NAME}`;
 
 class SQSService {
-  deliverEmail = (event, context, callback) => {
+  deliverEmail = async (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 
     try {
+      const now = new Date();
+      const emailLog = await EmailMessageLog.findById(JSON.parse(event.Records[0].body).emailLogId).exec();
+      emailLog.set({
+        initialRequestType: JSON.parse(event.Records[0].body).type,
+        initialRequestDate: now,
+        initialRequest: event.Records[0].body,
+        generateEmailMessageDate: now,
+        generateEmailMessage: JSON.parse(event.Records[0].body).message,
+        deliverEmailMessageDate: now,
+        deliverEmailMessage: now,
+      });
+      emailLog.save();
+
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
         to: 'team-texvet@inventive.io',
         from: 'test@example.com',
         subject: 'Sending with SendGrid is Fun',
-        text: JSON.parse(event.Records[0].body).text,
+        text: JSON.parse().text,
         html: `<div>${JSON.parse(event.Records[0].body).text}</div`,
       };
       sgMail.send(msg);
@@ -39,12 +53,29 @@ class SQSService {
       const template = await NotificationTemplateModel
         .findOne({ _id: mongoose.Types.ObjectId(type.template_id) })
         .exec();
+
+      const generatedTemplate = Mustache.render(template, { message: JSON.parse(event.Records[0].body).text });
+      const now = new Date();
+
+      const emailLog = await EmailMessageLog.findById(JSON.parse(event.Records[0].body).emailLogId).exec();
+      emailLog.set({
+        initialRequestType: type,
+        initialRequestDate: now,
+        initialRequest: event.Records[0].body,
+        generateEmailMessageDate: now,
+        generateEmailMessage: generatedTemplate,
+        deliverEmailMessageDate: null,
+        deliverEmailMessage: null,
+      });
+      emailLog.save();
+
       const params = {
         MessageBody: {
           subject: '',
-          message: Mustache.render(template, { message: JSON.parse(event.Records[0].body).text }),
+          message: generatedTemplate,
         },
         QueueUrl: QUEUE_URL,
+        emailLogId: emailLog._id,
       };
 
       sqs.sendMessage(params, (err, data) => {
