72bcabb27ef0cf3e740a98e9701c24b02c91405b
Eugene Belkovich <eugene.belkovich@gmail.com>
2019-05-24 16:54:14 +0300

    templates update changed

--- templates.js
+++ templates.js
@@ -1,21 +1,19 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
-
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
 const { minify } = require('html-minifier');
+const _ = require('lodash');
 const yamlConfig = require('node-yaml-config');
 const NotificationTemplate = require('./src/models/notificationTemplate');
-
+const { diff } = require('./src/utils/filter');
 require('./config');
 
 const readFile = util.promisify(fs.readFile);
 
 const SUBJECTS_PATH = './src/subjects';
 const TEMPLATES_PATH = './src/templates';
-
-
 const envConfig = yamlConfig.load('./env.yml');
 const mongoDB = envConfig.MONGODB_URI;
 
@@ -23,44 +21,43 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', async () => {
   console.log('Connected to MongoDB database');
 
-  fs.readdirSync(SUBJECTS_PATH)
-    .forEach(async subjectName => {
-      try {
-        const data = await readFile(path.join(SUBJECTS_PATH, subjectName), { encoding: 'utf-8' });
-        const template = NotificationTemplate.findOne({ name: subjectName });
-        template.subject = data;
-        template.save();
-      } catch (err) {
-        console.log('Subjects update Error: ', err);
-      }
-    });
+  const subjects = fs.readdirSync(SUBJECTS_PATH);
+  const templates = fs.readdirSync(TEMPLATES_PATH);
 
-  fs.readdirSync(TEMPLATES_PATH)
-    .forEach(async templateName => {
-      try {
-        const data = await readFile(path.join(TEMPLATES_PATH, templateName), { encoding: 'utf-8' });
-        const template = NotificationTemplate.findOne({ name: templateName });
-        template.template = minify(data, {
-          collapseWhitespace: true,
-          removeComments: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeTagWhitespace: true,
-          useShortDoctype: true,
-          minifyCss: true,
-          minifyJs: true,
-        });
-        template.save();
-      } catch (err) {
-        console.log('Templates update Error: ', err);
-      }
-    });
+  const difference = diff(subjects, templates);
+  if (!_.isEmpty(difference)) {
+    throw new Error(`Missing a subject or template Error: ${difference}`);
+  }
+
+  const intersection = _.intersection(subjects, templates);
+
+  await NotificationTemplate.deleteMany({ name: { $nin: intersection } }).exec();
+
+  intersection.forEach(async name => {
+    try {
+      const subjectData = await readFile(path.join(SUBJECTS_PATH, name), { encoding: 'utf-8' });
+      const templateData = await readFile(path.join(TEMPLATES_PATH, name), { encoding: 'utf-8' });
+      const minifiedTemplate = minify(templateData, {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeTagWhitespace: true,
+        useShortDoctype: true,
+        minifyCss: true,
+        minifyJs: true,
+      });
+      await NotificationTemplate.update({ name }, { subject: subjectData, template: minifiedTemplate }, {
+        upsert: true,
+        setDefaultsOnInsert: true,
+      }).exec();
+    } catch (err) {
+      console.log('Template update Error: ', err);
+    }
+  });
 });
-
-
