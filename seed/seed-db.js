4a9db3e0ee0574f5150f2105bebdeefe4f5b83c2
VladMogwai <vlad.arefiev@gmail.com>
2019-05-29 16:39:03 +0300

    removed redundant aws constant

--- seed/seed-db.js
+++ seed/seed-db.js
@@ -5,17 +5,14 @@ const { Seeder } = require('mongo-seeding');
 const yamlConfig = require('node-yaml-config');
 
 const path = require('path');
-const { AWS_PROFILE } = require('../config');
 require('../config');
 
 const dataPath = path.resolve(__dirname, 'data');
 
 function seedDb() {
-  process.env.AWS_PROFILE = AWS_PROFILE;
   const envConfig = yamlConfig.load(path.resolve(__dirname, '../env.yml'));
   const config = {
     database: envConfig.MONGODB_URI,
-    dropDatabase: true,
     dropCollections: true,
   };
 
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,2 @@
 export const DEFAULT_QUERY_LIMIT = 50;
 export const MAXIMUM_QUERY_LIMIT = 500;
-export const AWS_PROFILE = 'texvet';
