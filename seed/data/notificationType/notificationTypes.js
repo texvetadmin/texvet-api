12baefb783862494f430154b00b543e4cdcde5a9
VladMogwai <vlad.arefiev@gmail.com>
2019-06-04 17:42:25 +0300

    notificationType changed

--- seed/data/notificationType/notificationTypes.js
+++ seed/data/notificationType/notificationTypes.js
@@ -3,12 +3,30 @@ module.exports = [
     code: 'SET-APPOINTMENT',
     name: 'Activity report notification',
     description: 'Test notification type',
-    template_name: 'activity',
+    template_name: 'activity-report',
+  },
+  {
+    code: 'APPOINTMENT-REMINDER',
+    name: 'FollowUp notification notification',
+    description: 'Test notification type',
+    template_name: 'follow-up',
+  },
+  {
+    code: 'APPOINTMENT-REMINDER',
+    name: 'They want a human v1 notification',
+    description: 'They want a human v1 notification type',
+    template_name: 'they-want-a-human-v1',
+  },
+  {
+    code: 'APPOINTMENT-REMINDER',
+    name: 'They want a human v2 notification',
+    description: 'They want a human v2 notification type',
+    template_name: 'they-want-a-human-v2',
   },
   {
     code: 'APPOINTMENT-REMINDER',
     name: 'Transcript notification',
-    description: 'Test notification type',
+    description: 'Transcript notification type',
     template_name: 'transcript',
   },
 ];
