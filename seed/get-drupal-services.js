ba5e59c447c77de02a43a1e54ec9d13777b4fdaa
VladMogwai <vlad.arefiev@gmail.com>
2019-06-05 17:21:30 +0300

    retrieve categories instead of services

--- seed/get-drupal-services.js
+++ seed/get-drupal-services.js
@@ -5,26 +5,31 @@ const resolve = require('path').resolve;
 const collectionsPath = resolve(__dirname, 'data', 'services');
 
 const drupalUrl = process.env.DRUPAL_URL || 'http://inventive-d8-txc.pantheonsite.io';
-const url = `${drupalUrl}/rest/v1/fulfillments/services`;
+const url = `${drupalUrl}/rest/v1/content/resources/categories`;
 
 const getServices = async () => {
   const resp = await fetch(url);
   const response = await resp.json();
 
-  const servicesSlugList = response.reduce((list, service) => {
-    service.field_org_tags.map(s => {
-      const slug = s.url ? s.url.split('/')[2].toUpperCase() : null;
-      if (!list[slug]) {
-        list[slug] = s.target_id;
-      }
-    });
-    return list;
-  }, {});
+  const categories = await response.map(category => {
+    const id = category.tid[0] ? category.tid[0].value : null;
+    const uuid = category.uuid[0] ? category.uuid[0].value : null;
+    const title = category.name[0] ? category.name[0].value : null;
+    const url = category.path[0] ? category.path[0].alias : null;
+    const slug = url ? url.split('/')[2] : null;
+    return {
+      target_id: id,
+      target_uuid: uuid,
+      url,
+      title,
+      slug,
+    }
+  });
 
   if (!fs.existsSync(collectionsPath)) {
     fs.mkdirSync(collectionsPath);
   }
-  const content = `module.exports = ${JSON.stringify(servicesSlugList)}`;
+  const content = `module.exports = ${JSON.stringify(categories)}`;
   fs.writeFile(`${collectionsPath}/data.js`, content, error => {
     if (error) {
       throw error;
@@ -33,16 +38,4 @@ const getServices = async () => {
 };
 getServices();
 
-//   const services = await response.map(service => ({
-//     id: service.nid[0] ? service.nid[0].value : null,
-//     uuid: service.uuid[0] ? service.uuid[0].value : null,
-//     title: service.title[0] ? service.title[0].value : null,
-//     email: service.field_email[0] ? service.field_email[0].value : null,
-//     org_tags: service.field_org_tags.map(tag => {
-//       const slug = tag.url ? tag.url.split('/')[2].toUpperCase() : null;
-//       return {
-//         id: tag.target_id,
-//         slug,
-//       };
-//     }),
-//   }));
+
